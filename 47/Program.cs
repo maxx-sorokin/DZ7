f870af440b802a1803354336d534de7727e8134b
Maxim Sorokin <maximka773@mail.ru>
2022-11-24 16:01:22 +0300

    47 done

---
+++ 47/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+// Сделать отдельный метод который генерирует двумерный массив
+// и отдельный метод который выводит его на экран.
+// m и n вводит пользователь в консоль.
+
+Console.Write("Введите количество строк m: ");
+bool isParsedM = int.TryParse(Console.ReadLine(), out int m);
+Console.Write("Введите количество столбцов n: ");
+bool isParsedN = int.TryParse(Console.ReadLine(), out int n);
+
+if (!isParsedM || !isParsedN)
+{
+    Console.WriteLine("Ошибка!");
+    return;
+}
+
+double[,] array = CreateRandom2DArray(m, n);
+Print2DArray(array);
+
+double[,] CreateRandom2DArray(int m, int n)
+{
+    double[,] array = new double[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(1, 100) + Math.Round(random.NextDouble(), 2);
+        }
+    }
+    return array;
+}
+
+void Print2DArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
